15f34e69987caa0b5b45821c1d9538e92809c3c1
listn <186027681+Klez-src@users.noreply.github.com>
2025-05-01 22:48:12 +0100

    :p

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('button').addEventListener('click', function() {
   const name = document.querySelector('input[type="text"]').value;
-  const password = document.querySelector('input[type="password"]').value;
+  const email = document.querySelector('input[type="email"]').value;
   const message = document.querySelector('textarea').value;
-  console.log('Name:', name, 'Password:', password, 'Message:', message);
-  alert('Form submitted!\nName: ' + name + '\nMessage: ' + message);
+  console.log('Name:', name, 'Email:', email, 'Message:', message);
+  alert('Form submitted!\nName: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
   document.querySelector('input[type="text"]').value = '';
-  document.querySelector('input[type="password"]').value = '';
+  document.querySelector('input[type="email"]').value = '';
   document.querySelector('textarea').value = '';
 });
